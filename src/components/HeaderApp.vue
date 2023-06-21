24680a77d4541cd98ca58ce25f13a2a9f38c7111
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-21 11:55:02 +0200

    added nav component that contains searchbar that sends its value to header parent

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,14 +1,22 @@
 <script>
+    import HeaderNavbar from './HeaderNavbar.vue';
     export default{
         name: 'HeaderApp',
         components: {
-            
+            HeaderNavbar,
+        },
+        methods: {
+            displayFilm(filmName){
+                console.log(filmName)
+            }
         }
     }
 </script>
 
 <template>
-    
+    <header>
+        <HeaderNavbar @lookingFilm="displayFilm"/>
+    </header>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/HeaderNavbar.vue
@@ -0,0 +1,24 @@
+<script>
+    export default{
+        name: 'HeaderNavbar',
+        data() {
+            return {
+                searchValue: '',
+            }
+        },
+    }
+</script>
+
+<template>
+    <nav>
+        <h1>Logo</h1>
+        <div class="ivy_search_box">
+            <input type="text" v-model="searchValue">
+            <button @click="$emit('lookingFilm', searchValue), searchValue = ''">GO</button>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
