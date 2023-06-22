d84405ff9352ea34a0db41d55033f8aa16680592
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-22 11:09:22 +0200

    addded info film on card cover hover

--- src/components/MainFilmList.vue
+++ src/components/MainFilmList.vue
@@ -28,35 +28,35 @@ import { store } from '../store.js'
 </script>
 
 <template>
+    <h1 class="ivy_section_title">Movies</h1>
     <ul class="ivy_film_grid">
         <li v-for="film in store.filmList">
-            <ul>
-                <li>
-                    <img :src="(film.backdrop_path != null) 
+            <div class="ivy_movie_cover">
+                <img :src="(film.backdrop_path != null) 
                     ? getImagePath(`https://image.tmdb.org/t/p/w342${film.backdrop_path}`) 
                     : getImagePath(`../assets/default_poster.jpg`)" 
-                    :alt="film.original_name + 'image'"
-                    class="ivy_movie_cover">
-                </li>
-                <!-- <li>
-                    {{ film.title }}
-                </li>
-                <li>
-                    {{ film.original_title }}
-                </li> -->
-                <!-- <li>
+                    :alt="film.original_name + 'image'">
+                <div class="ivy_movie_info">
+                    <h2 class="ivy_movie_title">
+                        {{ film.title }}
+                    </h2>
                     <img :src="isAvaibleLang(film.original_language) ? getImagePath(`../assets/lang/${film.original_language}.png`) : getImagePath(`../assets/lang/globe.png`)" 
-                    :alt="film.original_language + ' language'" >
-                </li> -->
-                <!-- <li>
-                    <span v-for="star in parseInt(handleVote(film.vote_average))">
-                        <i class="fa-solid fa-star"></i>
-                    </span>
-                    <span v-for="unstar in (5 - parseInt(handleVote(film.vote_average)))">
-                        <i class="fa-regular fa-star"></i>
-                    </span>
-                </li> -->
-            </ul>
+                    :alt="film.original_language + ' language'" 
+                    class="ivy_movie_lang" 
+                    :class="isAvaibleLang(film.original_language) ? '' : 'ivy_filter_white'">
+                    <p class="ivy_movie_desc">
+                        {{ film.overview }}
+                    </p>
+                    <div class="ivy_movie_stars">
+                        <span v-for="star in parseInt(handleVote(film.vote_average))">
+                            <i class="fa-solid fa-star"></i>
+                        </span>
+                        <span v-for="unstar in (5 - parseInt(handleVote(film.vote_average)))">
+                            <i class="fa-regular fa-star"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </li>
     </ul>
 </template>
@@ -70,12 +70,55 @@ import { store } from '../store.js'
         .ivy_movie_cover {
             height: 600px;
             width: 342px;
-            object-fit: cover;
-            object-position: center;
-        }
-    
-        .fa-star{
-            color: gold;
+            position: relative;
+            cursor: pointer;
+
+            img{
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                object-position: center;
+            }
+
+            .ivy_movie_info{
+                position: absolute;
+                top: 0;
+                opacity: 0;
+                background-color: rgba(0, 0, 0, 0.7);
+                transition: all 0.33s ease;
+                height: 100%;
+                width: 100%;
+                padding: 2rem 1rem;
+
+                .ivy_movie_title{
+                    color: red;
+                    font-size: 1.3rem;
+                    text-align: center;
+                }
+
+                .ivy_movie_lang{
+                    display: block;
+                    height: 20px;
+                    object-fit: contain;
+                    margin: 1rem 0;
+                }
+
+                .ivy_movie_desc{
+                    height: 400px;
+                    overflow: hidden;
+                }
+
+                .ivy_movie_stars{
+                    position: absolute;
+                    bottom: 20px;
+                    left: 50%;
+                    transform: translateX(-50%);
+                }
+            }
+
+            &:hover .ivy_movie_info{
+                opacity: 1;
+            }
         }
     }
 </style>
--- src/components/MainSeriesList.vue
+++ src/components/MainSeriesList.vue
@@ -21,39 +21,42 @@ import { store } from '../store.js'
                 if(vote > 0){
                     return (vote / 2).toFixed()
                 }
-                return 'N.C.'
+                return 0;
             }
         }
     }
 </script>
 
 <template>
+    <h1 class="ivy_section_title">Series</h1>
     <ul class="ivy_serie_grid">
         <li v-for="serie in store.seriesList">
-            <ul>
-                <li>
-                    <img :src="(serie.backdrop_path != null) 
+            <div class="ivy_serie_cover">
+                <img :src="(serie.backdrop_path != null) 
                     ? getImagePath(`https://image.tmdb.org/t/p/w342${serie.backdrop_path}`) 
                     : getImagePath(`../assets/default_poster.jpg`)" 
-                    :alt="serie.original_name + 'image'"
-                    class="ivy_serie_cover">                
-                </li>
-                <!-- <li>
-                    {{ serie.original_name }}
-                </li> -->
-                <!-- <li>
+                    :alt="serie.original_name + 'image'">  
+                <div class="ivy_serie_info">
+                    <h1 class="ivy_serie_title">
+                        {{ serie.original_name }}
+                    </h1>
                     <img :src="isAvaibleLang(serie.original_language) ? getImagePath(`../assets/lang/${serie.original_language}.png`) : getImagePath(`../assets/lang/globe.png`)" 
-                    :alt="serie.original_language + ' language'" >
-                </li> -->
-                <!-- <li>
-                    <span v-for="star in parseInt(handleVote(serie.vote_average))">
-                        <i class="fa-solid fa-star"></i>
-                    </span>
-                    <span v-for="unstar in (5 - parseInt(handleVote(serie.vote_average)))">
-                        <i class="fa-regular fa-star"></i>
-                    </span>
-                </li> -->
-            </ul>
+                    :alt="serie.original_language + ' language'" 
+                    class="ivy_serie_lang"
+                    :class="isAvaibleLang(serie.original_language) ? '' : 'ivy_filter_white'">
+                    <p class="ivy_serie_desc">
+                        {{ serie.overview }}
+                    </p>
+                    <div class="ivy_serie_stars">
+                        <span v-for="star in parseInt(handleVote(serie.vote_average))">
+                            <i class="fa-solid fa-star"></i>
+                        </span>
+                        <span v-for="unstar in (5 - parseInt(handleVote(serie.vote_average)))">
+                            <i class="fa-regular fa-star"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </li>
     </ul>
 </template>
@@ -64,15 +67,60 @@ import { store } from '../store.js'
     @include flex(row, center, flex-start, wrap);
     width: 100vw;
     gap: 1rem;
+
         .ivy_serie_cover {
             height: 600px;
             width: 342px;
-            object-fit: cover;
-            object-position: center;
-        }
-    
-        .fa-star{
-            color: gold;
+            position: relative;
+            cursor: pointer;
+            
+            img{
+                display: block;
+                height: 100%;
+                width: 100%;
+                object-fit: cover;
+                object-position: center;
+            }
+            .ivy_serie_info{
+                position: absolute;
+                top: 0;
+                opacity: 0;
+                background-color: rgba(0, 0, 0, 0.7);
+                transition: all 0.33s ease;
+                height: 100%;
+                width: 100%;
+                padding: 2rem 1rem;
+
+                .ivy_serie_title{
+                    color: red;
+                    font-size: 1.3rem;
+                    text-align: center;
+                }
+
+                .ivy_serie_lang{
+                    display: block;
+                    height: 20px;
+                    object-fit: contain;
+                    margin: 1rem 0;
+                }
+
+                .ivy_serie_desc{
+                    height: 400px;
+                    overflow: hidden;
+                }
+
+                .ivy_serie_stars{
+                    position: absolute;
+                    bottom: 20px;
+                    left: 50%;
+                    transform: translateX(-50%);
+
+                }
+            }
+
+            &:hover .ivy_serie_info{
+                opacity: 1;
+            }
         }
     }
 </style>
